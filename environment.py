a114bb5fe172c557c6946f82d0324ed64f9d5cc0
NatJ24 <nataliajimenez@uma.es>
2022-03-23 13:11:21 +0100

    commit

--- environment.py
+++ environment.py
@@ -1,9 +1,9 @@
 class Environment:
-    def __init__(self,lum):
-        self.lum = lum
+    def __init__(self):
+        self.lum = 50
     def modify_lum(self,variacion):
         self.lum += variacion   # la luminosidad tiene un valor y varía según indique el usuario
     def set_lum(self,value):
         self.lum = value    # el usuario indica el valor de luminosidad del ambiente
     def get_lum(self):
-        
+        return self.lum
